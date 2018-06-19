19601789a7b9e5acf26cbe277306f3abfcf12d57
浩瀚宇宙 <aaronchen98@outlook.com>
2018-06-19 17:02:03 +0800

    1

--- 1.py
+++ 1.py
@@ -5,6 +5,7 @@ from pylab import imshow
 from pylab import array
 from pylab import plot
 from pylab import title
+from pylab import show
 
 
 a = op.AlignDlib(
@@ -16,4 +17,23 @@ bgrImg = cv2.imread(imgPath)
 rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
 
 bb = a.getLargestFaceBoundingBox(rgbImg)
-print(a.findLandmarks(rgbImg, bb=bb))
+points = a.findLandmarks(rgbImg, bb=bb)
+
+
+im = array(Image.open('/Users/aaron/Desktop/1.jpg'))
+imshow(im)
+
+x = []
+y = []
+for item in points:
+    x.append(item[0])
+    y.append(item[1])
+
+
+# 使用红色星状标记绘制点
+plot(x, y, 'r*')
+# 绘制连接前两个点的线
+# plot(x[:2],y[:2])
+# 添加标题，显示绘制的图像
+title('Landmarks')
+show()
